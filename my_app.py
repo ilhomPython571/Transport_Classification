66b4c2b5d645841048f9ddbb446242218f5fde6b
ilhomPython571 <udemy.cyprus@gmail.com>
2025-08-01 14:51:44 +0500

    project files have been uploaded

---
+++ my_app.py
@@ -0,0 +1,31 @@
+import pathlib
+pathlib.PosixPath = pathlib.WindowsPath
+
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+
+
+# title
+st.title('Transportni klassifikatsiya qiluvchi model')
+
+# rasm joylash
+file = st.file_uploader('Rasm yuklash ', type=['png', 'jpeg', 'gif', 'svg'])
+if file:
+    st.image(file)
+
+    # PIL convert
+    img = PILImage.create(file)
+
+    # model
+    model = load_learner('transport_model.pkl')
+
+    # prediction
+    pred, pred_id, probs = model.predict(img)
+    st.success(f'Bashorat : {pred}')
+    st.info(f'Ehtimollik : {probs[pred_id]*100:.2f}')
+    
+    # plotting
+    fig = px.bar(x=probs, y=model.dls.vocab)
+    st.plotly_chart(fig)
+    
